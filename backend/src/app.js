89a4f28b258dd58b24e26d5f21e5982fb8d23dab
Aman Raza <officialamanraz@gmail.com>
2026-07-05 20:06:31 +0530

    Fixed CORS for live frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -115,5 +115,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running beautifully on PORT ${PORT} 🚀`);
 });
-
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'https://arzoo-engd.onrender.com' 
+  ],
+  credentials: true
+}));
 module.exports = app;
